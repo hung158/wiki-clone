ba6222fc0bf13960af1198a8f497c37ef3e0c992
hung158 <litruong158@gmail.com>
2024-10-15 02:42:53 -0700

    add tailwind config

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,39 @@
 <template>
-    <div class=" text-blue-600">
-        
-        This is Home page
+    <div class="w-full h-auto">
+        <section>
+            <div class="pt-14">
+                <div class="max-w-7xl mr-auto ml-auto pl-9 pr-9">
+                    <div class="w-[58.33333%] ml-auto mr-auto">
+                        <div class="pl-3 pr-3">
+                            <div>
+                                <div class="px-2 py-3">
+                                    <h2 class="">
+                                        <div>
+                                            <span class="font-sans font-bold text-[32px]">
+                                                📚
+                                            </span>
+                                        </div>
+                                       
+                                    </h2>
+                                    <h2 class="font-sans font-bold text-[32px]">
+                                        <div>
+                                            <span>
+                                                Sites.wiki
+                                            </span>
+                                        </div>
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
+
+<style scoped >
+.font-sans{
+    font-family:'Noto Emoji', Arial !important;
+} 
+</style>
